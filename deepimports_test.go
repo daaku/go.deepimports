618cd78cd6a65a41f900496d8622c4554ddf2072
Naitik Shah <n@daaku.org>
2013-02-15 17:16:15 -0800

    a little more testing

--- deepimports_test.go
+++ deepimports_test.go
@@ -5,13 +5,36 @@ import (
 	"testing"
 )
 
+const aPath = "github.com/daaku/go.deepimports/_test/a"
+const bPath = "github.com/daaku/go.deepimports/_test/b"
+
 func TestFind(t *testing.T) {
-	paths := []string{"github.com/daaku/go.deepimports/_test/a"}
+	paths := []string{aPath}
 	r, err := deepimports.Find(paths, "")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if len(r) != 2 {
 		t.Fatalf("expected 2 imports got %d", len(r))
+	}
+	if r[0].ImportPath != aPath {
+		t.Fatalf("expected %s got %s", aPath, r[0].ImportPath)
+	}
+	if r[1].ImportPath != bPath {
+		t.Fatalf("expected %s got %s", bPath, r[1].ImportPath)
+	}
+}
+
+func TestFindImportsOnly(t *testing.T) {
+	paths := []string{"github.com/daaku/go.deepimports/_test/a"}
+	r, err := deepimports.FindImportsOnly(paths, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(r) != 1 {
+		t.Fatalf("expected 1 imports got %d", len(r))
+	}
+	if r[0].ImportPath != bPath {
+		t.Fatalf("expected %s got %s", bPath, r[0].ImportPath)
 	}
 }
